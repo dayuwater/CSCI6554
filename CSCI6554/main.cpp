6b742463c59a09f39dda9bd5cc64156c27b95b7c
dayuwater <dayuwater@dayuwatertekiMacBook-Pro.local>
2015-01-25 15:51:30 -0500

    Initial Commit

---
+++ CSCI6554/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSCI6554
+//
+//  Created by dayuwater on 15/1/25.
+//  Copyright (c) 2015年 dayuwater. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
